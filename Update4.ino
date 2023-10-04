b769b9e0f5411a514e35184dfdef22013a90a07c
yreziua2020 <yreziua@gmail.com>
2023-10-04 10:39:04 +0300

    add ftp

--- Update4.ino
+++ Update4.ino
@@ -26,7 +26,13 @@ const char* password = STAPSK;
 ESP8266WebServer httpServer(80);
 ESP8266HTTPUpdateServer httpUpdater;
 
+#include <ESP8266FtpServer.h> 
+FtpServer ftpSrv; //для фтп
+
+
 void setup(void) {
+
+
 
   Serial.begin(9600);  Serial.println();  Serial.println("Booting Sketch...");
   
@@ -38,13 +44,19 @@ void setup(void) {
 
   //MDNS.begin(host);
 
-  httpUpdater.setup(&httpServer); 
+  httpUpdater.setup(&httpServer);     //для обновления
   httpServer.begin();
+
+  ftpSrv.begin("admin","");                                        // Поднимаем FTP-сервер для удобства отладки работы HTML (логин: relay, пароль: relay)
 
  // MDNS.addService("http", "tcp", 80);  Serial.printf("HTTPUpdateServer ready! Open http://%s.local/update in your browser\n", host);
 }
 
 void loop(void) {
   httpServer.handleClient();
+    delay(1);
+    ftpSrv.handleFTP();                                                 // Обработчик FTP-соединений  
+    delay(1);
+
   //MDNS.update();
 }
